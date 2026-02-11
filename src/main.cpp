9bee90b18fc0579ae4a569dac6aa6549c9434d57
nathan <wrupg2joj9@gmail.com>
2026-02-11 11:28:44 +0800

    init: PID and Encoder_filter File

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+  int a=1;
+  int b= 2;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+  Serial.println(a+b);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
