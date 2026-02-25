a10e6297bf8fb6b862fde82a58a5a3d1d4a27fe7
nathan <wrupg2joj9@gmail.com>
2026-02-25 23:34:16 +0800

    add: PID first version

--- include/config.h
+++ include/config.h
@@ -1,5 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
+#pragma once
+#include <Arduino.h>
 // ========== 腳位定義 ==========
 
 // Encoder
@@ -13,6 +15,22 @@
 // ========== 各參數調配 ==========
 
 // PID參數
+
+namespace PidConfig {
+    // 縮放因子
+    constexpr uint8_t SCALE_SHIFT = 8;           // 2^8 = 256
+    constexpr int16_t SCALE_FACTOR = (1 << SCALE_SHIFT); // 256
+
+    // 浮點參數 → 轉換時使用
+    constexpr float LEFT_KP  = 2.0f;
+    constexpr float LEFT_KI  = 0.5f;
+    constexpr float LEFT_KD  = 0.1f;
+
+    // 輸出限制
+    constexpr int16_t OUTPUT_MAX      =  255;
+    constexpr int16_t OUTPUT_MIN      = -255;
+    constexpr int16_t INTEGRAL_LIMIT  =  100 * SCALE_FACTOR;
+}
 
 // 濾波參數
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,49 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+
 // 自訂的函式庫
 #include "Hardware_Layer.h"
 #include "Math_Layer.h" 
 #include "Comms_Layer.h"
+#include "config.h"
 
+
+const int motorPWM = 9;
+const int motorDirection =8;
+const int motorDirection2 =7;
+const int STBY =6;
+
+long count =0;
+void motorCNT() {
+  count++;
+}
 
 void setup() {
 
   Serial.begin(115200);
-  initTimer1_20ms();
 
+  attachInterrupt(digitalPinToInterrupt(2), motorCNT, RISING); 
+  pinMode(motorPWM, OUTPUT);
+  pinMode(motorDirection, OUTPUT);
+  pinMode(motorDirection2, OUTPUT);
+  pinMode(STBY, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+
+  initTimer1_20ms();
+  PID_Controller pid(PidConfig::LEFT_KP, PidConfig::LEFT_KI, PidConfig::LEFT_KD);
 }
 
 void loop() {
+
+  digitalWrite(STBY,HIGH);
+  digitalWrite(motorDirection,HIGH);
+  digitalWrite(motorDirection2,LOW);
+  delay(2000);
+  analogWrite(motorPWM, 100);
   if (timerFlag){
     timerFlag = false;
-    Serial.println(millis());
-
+    Serial.println(count);
   }
 }
 
