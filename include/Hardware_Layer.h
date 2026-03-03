f7ac2721e8faa1520b0452b9136baabe2ca4179f
nathan <wrupg2joj9@gmail.com>
2026-03-03 18:06:26 +0800

    add: Motor method

--- include/Hardware_Layer.h
+++ include/Hardware_Layer.h
@@ -10,15 +10,19 @@ void initTimer1_20ms(); // 初始化函式的宣告
 
 class Motor{
     public:
-        Motor(float Kp, float Ki, float Kd, uint8_t pwmPin, uint8_t dirPin1, uint8_t dirPin2, uint8_t encoderPin);
+        Motor(float Kp, float Ki, float Kd, uint8_t pwmPin, uint8_t dirPin1, uint8_t dirPin2, uint8_t encoderPin1, uint8_t encoderPin2);
         void setSpeed(int16_t speed);
-        void stop();
+        void updateEncoder();
+        void encoderISR();
     private:
         uint8_t pwmPin;
         uint8_t dirPin1;
         uint8_t dirPin2;
-        uint8_t encoderPin;
+        uint8_t encoderPin1;
+        uint8_t encoderPin2;
         PID_Controller speedPID;
+        volatile uint16_t encoderCount = 0;
+        int16_t currentSpeed = 0;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,12 @@
 #include "config.h"
 
 
-const int motorPWM = 9;
-const int motorDirection =8;
-const int motorDirection2 =7;
-const int STBY =6;
 
-long count =0;
-void motorCNT() {
-  count++;
-}
+//Motor(Kp, Ki, Kd, pwmPin, dirPin1, dirPin2, encoderPin)
 
 void setup() {
 
   Serial.begin(115200);
-
-  attachInterrupt(digitalPinToInterrupt(2), motorCNT, RISING); 
-  pinMode(motorPWM, OUTPUT);
-  pinMode(motorDirection, OUTPUT);
-  pinMode(motorDirection2, OUTPUT);
-  pinMode(STBY, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
 
   initTimer1_20ms();
   PID_Controller pid(PidConfig::PID_KP, PidConfig::PID_KI, PidConfig::PID_KD);
@@ -37,14 +23,10 @@ void setup() {
 
 void loop() {
 
-  digitalWrite(STBY,HIGH);
-  digitalWrite(motorDirection,HIGH);
-  digitalWrite(motorDirection2,LOW);
-  delay(2000);
-  analogWrite(motorPWM, 100);
+
   if (timerFlag){
     timerFlag = false;
-    Serial.println(count);
+    //update encoder
   }
 }
 
