582cb7365d0186408a73ab7e6af794406c23c57c
nathan <wrupg2joj9@gmail.com>
2026-03-03 15:39:56 +0800

    feat: PID compute

--- include/config.h
+++ include/config.h
@@ -18,18 +18,18 @@
 
 namespace PidConfig {
     // 縮放因子
-    constexpr uint8_t SCALE_SHIFT = 8;           // 2^8 = 256
-    constexpr int16_t SCALE_FACTOR = (1 << SCALE_SHIFT); // 256
-
-    // 浮點參數 → 轉換時使用
-    constexpr float LEFT_KP  = 2.0f;
-    constexpr float LEFT_KI  = 0.5f;
-    constexpr float LEFT_KD  = 0.1f;
+    constexpr uint8_t SCALE_SHIFT = 10;           // 2^10 = 1024
+    constexpr int16_t SCALE_FACTOR = (1 << SCALE_SHIFT); // 1024
+    
+    constexpr float timeStep = 0.02f; //s
+    constexpr float PID_KP  = 2.0f;
+    constexpr float PID_KI  = 0.5f;
+    constexpr float PID_KD  = 0.1f;
 
     // 輸出限制
     constexpr int16_t OUTPUT_MAX      =  255;
     constexpr int16_t OUTPUT_MIN      = -255;
-    constexpr int16_t INTEGRAL_LIMIT  =  100 * SCALE_FACTOR;
+    constexpr int32_t INTEGRAL_LIMIT  =  128 * SCALE_FACTOR;
 }
 
 // 濾波參數
