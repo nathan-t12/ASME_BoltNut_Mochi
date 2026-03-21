c7ff6f2a8fd01d33226f677ecee5e4a90edaaff0
nathan <wrupg2joj9@gmail.com>
2026-03-21 21:40:07 +0800

    testing 4 motor success

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,25 @@ IBusBM ibus;
 // 1: 開迴路 (C1 -> 直接 PWM)
 #define MOTOR_TEST_MODE 1
 
+Motor motor1(PidConfig::PID_KP, PidConfig::PID_KI, PidConfig::PID_KD,
+             MotorPins::M1_PWM_PIN, MotorPins::M1_DIR_PIN1, MotorPins::M1_DIR_PIN2, MotorPins::M1_ENCODER_PIN1, MotorPins::M1_ENCODER_PIN2);
+
+Motor motor2(PidConfig::PID_KP, PidConfig::PID_KI, PidConfig::PID_KD,
+             MotorPins::M2_PWM_PIN, MotorPins::M2_DIR_PIN1, MotorPins::M2_DIR_PIN2, MotorPins::M2_ENCODER_PIN1, MotorPins::M2_ENCODER_PIN2);
+
 Motor motor3(PidConfig::PID_KP, PidConfig::PID_KI, PidConfig::PID_KD,
              MotorPins::M3_PWM_PIN, MotorPins::M3_DIR_PIN1, MotorPins::M3_DIR_PIN2, MotorPins::M3_ENCODER_PIN1, MotorPins::M3_ENCODER_PIN2);
 
 Motor motor4(PidConfig::PID_KP, PidConfig::PID_KI, PidConfig::PID_KD,
              MotorPins::M4_PWM_PIN, MotorPins::M4_DIR_PIN1, MotorPins::M4_DIR_PIN2, MotorPins::M4_ENCODER_PIN1, MotorPins::M4_ENCODER_PIN2);
+
+void motor1EncoderISR() {
+    motor1.encoderISR();
+}
+
+void motor2EncoderISR() {
+    motor2.encoderISR();
+}
 
 void motor3EncoderISR() {
     motor3.encoderISR();
@@ -29,6 +43,8 @@ uint32_t lastPrintMs = 0;
 uint16_t lastCh1 = CommsMapConfig::C1_CENTER;
 int16_t lastTargetCount = 0;
 int16_t lastPwmCmd = 0;
+int16_t lastSpeedOL1 = 0;
+int16_t lastSpeedOL2 = 0;
 int16_t lastSpeedOL3 = 0;
 int16_t lastSpeedOL4 = 0;
 
@@ -36,6 +52,8 @@ void setup() {
     Serial.begin(115200);
     ibus.begin(Serial2);
 
+    motor1.begin();
+    motor2.begin();
     motor3.begin();
     motor4.begin();
     initTimer1_20ms();
@@ -43,13 +61,15 @@ void setup() {
     pinMode(MotorPins::STBY1, OUTPUT);
     pinMode(MotorPins::STBY2, OUTPUT);
 
+    attachInterrupt(digitalPinToInterrupt(MotorPins::M1_ENCODER_PIN1), motor1EncoderISR, RISING);
+    attachInterrupt(digitalPinToInterrupt(MotorPins::M2_ENCODER_PIN1), motor2EncoderISR, RISING);
     attachInterrupt(digitalPinToInterrupt(MotorPins::M3_ENCODER_PIN1), motor3EncoderISR, RISING);
     attachInterrupt(digitalPinToInterrupt(MotorPins::M4_ENCODER_PIN1), motor4EncoderISR, RISING);
     digitalWrite(MotorPins::STBY1, HIGH);
     digitalWrite(MotorPins::STBY2, HIGH);
 
     delay(100);
-    Serial.println("Motor3+Motor4 Test Started (C1 -> command -> M3/M4)");
+    Serial.println("Motor1~Motor4 Test Started (C1 -> command -> M1/M2/M3/M4)");
 }
 
 void loop() {
@@ -69,13 +89,17 @@ void loop() {
 
 #if MOTOR_TEST_MODE == 0
         if (lastTargetCount == 0) {
+            motor1.stopAndResetControl();
+            motor2.stopAndResetControl();
             motor3.stopAndResetControl();
             motor4.stopAndResetControl();
             lastPwmCmd = 0;
         } else {
+            motor1.updateEncoder(lastTargetCount);
+            motor2.updateEncoder(lastTargetCount);
             motor3.updateEncoder(lastTargetCount);
             motor4.updateEncoder(lastTargetCount);
-            lastPwmCmd = (motor3.getLastPWM() + motor4.getLastPWM()) / 2;
+            lastPwmCmd = (motor1.getLastPWM() + motor2.getLastPWM() + motor3.getLastPWM() + motor4.getLastPWM()) / 4;
         }
 #else
         // 開迴路：用 CH1 直接控制 PWM，先確認馬達與驅動硬體路徑正常
@@ -89,15 +113,21 @@ void loop() {
                              PidConfig::OUTPUT_MIN,
                              PidConfig::OUTPUT_MAX);
         }
+        motor1.setSpeed(lastPwmCmd);
+        motor2.setSpeed(lastPwmCmd);
         motor3.setSpeed(lastPwmCmd);
-                    motor4.setSpeed(lastPwmCmd);
+        motor4.setSpeed(lastPwmCmd);
 
         // 開迴路下仍更新回授，便於觀察速度是否有變化
+        motor1.updateFeedbackOnly();
+        motor2.updateFeedbackOnly();
         motor3.updateFeedbackOnly();
-                    motor4.updateFeedbackOnly();
+        motor4.updateFeedbackOnly();
 
-                    lastSpeedOL3 = motor3.getCurrentSpeed();
-                    lastSpeedOL4 = motor4.getCurrentSpeed();
+        lastSpeedOL1 = motor1.getCurrentSpeed();
+        lastSpeedOL2 = motor2.getCurrentSpeed();
+        lastSpeedOL3 = motor3.getCurrentSpeed();
+        lastSpeedOL4 = motor4.getCurrentSpeed();
 #endif
     }
 
@@ -111,11 +141,23 @@ void loop() {
         Serial.print(",CmdPWM:");
         Serial.print(lastPwmCmd);
     #if MOTOR_TEST_MODE == 1
+        Serial.print(",Speed1(OL):");
+        Serial.print(lastSpeedOL1);
+        Serial.print(",Speed2(OL):");
+        Serial.print(lastSpeedOL2);
         Serial.print(",Speed3(OL):");
         Serial.print(lastSpeedOL3);
         Serial.print(",Speed4(OL):");
         Serial.print(lastSpeedOL4);
     #endif
+        Serial.print(",Speed1:");
+        Serial.print(motor1.getCurrentSpeed());
+        Serial.print(",PWM1:");
+        Serial.print(motor1.getLastPWM());
+        Serial.print(",Speed2:");
+        Serial.print(motor2.getCurrentSpeed());
+        Serial.print(",PWM2:");
+        Serial.print(motor2.getLastPWM());
         Serial.print(",Speed3:");
         Serial.print(motor3.getCurrentSpeed());
         Serial.print(",PWM3:");
